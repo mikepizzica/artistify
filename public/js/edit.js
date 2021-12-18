0bec43c27da78fd3355bc4226bddca55d5d6570c
Mike Pizzica <mike.pizzica@gmail.com>
2021-12-18 08:02:41 -0500

    add minor styling changes. update edit.js to reference updated button.

--- public/js/edit.js
+++ public/js/edit.js
@@ -6,7 +6,7 @@ const newFormHandler = async (event) => {
   const spotify_embed_code = document.querySelector('#spotify-embed-code').value.trim();
   const artist_img = document.querySelector('#artist-img').value.trim();
   const twitter_embed_code = document.querySelector('#twitter-embed-code').value.trim();
-  const project_id = document.getElementsByClassName('btn hover:bg-green-600 hover:text-green-200 bg-red-100 text-white-700 mx-5 my-10 px-6 py-2 rounded-lg')[0].id;
+  const project_id = document.getElementsByClassName('btn hover:bg-green-600 hover:text-green-200 text-white-700 mx-5 my-10 px-6 py-2 rounded-lg text-5xl')[0].id;
 
   // accept user input and edit existing info in DB
   if (name && artist_bio && spotify_embed_code && artist_img && twitter_embed_code) {
